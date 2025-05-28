aa2f24934598f429b321452c82735ba26a4809ac
HermesRhoDev <materrazzi.pro@gmail.com>
2025-05-28 20:41:21 +0200

    add workflow github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
@@ -9,7 +9,14 @@ const config = {
 			}
 		}
 	}),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : '/yotokoh'
+		}
+	}
 };
 
 export default config;
